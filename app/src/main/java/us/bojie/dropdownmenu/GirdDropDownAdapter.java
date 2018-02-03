b3e457fd8c0f93ccf5a15b99196b4d82ad0ef9b5
Bojie Jiang <jiangbojie@gmail.com>
2018-02-03 00:01:14 -0800

    GirdDropDownAdapter

--- app/src/main/java/us/bojie/dropdownmenu/GirdDropDownAdapter.java
+++ app/src/main/java/us/bojie/dropdownmenu/GirdDropDownAdapter.java
@@ -1,9 +1,11 @@
 package us.bojie.dropdownmenu;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -15,7 +17,7 @@ public class GirdDropDownAdapter extends BaseAdapter {
 
     private Context mContext;
     private List<String> mList;
-    private int mCheckItemPosition = 0;
+    private int mCheckItemPosition = -1;
 
     public GirdDropDownAdapter(Context context, List<String> list) {
         mContext = context;
@@ -26,7 +28,6 @@ public class GirdDropDownAdapter extends BaseAdapter {
         mCheckItemPosition = position;
         notifyDataSetChanged();
     }
-
 
 
     @Override
@@ -46,6 +47,38 @@ public class GirdDropDownAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        ViewHolder viewHolder;
+        if (convertView != null) {
+            viewHolder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_list_drop_down, parent);
+            viewHolder = new ViewHolder();
+            viewHolder.tv = convertView.findViewById(R.id.tv);
+            convertView.setTag(viewHolder);
+        }
+        fillView(position, viewHolder);
+        return convertView;
+    }
+
+    private void fillView(int position, ViewHolder viewHolder) {
+        viewHolder.tv.setText(mList.get(position));
+        if (mCheckItemPosition != -1) {
+            if (mCheckItemPosition == position) {
+                viewHolder.tv.setTextColor(mContext.getResources().getColor(R.color.selected));
+                viewHolder.tv.setCompoundDrawablesWithIntrinsicBounds(
+                        null, null,
+                        mContext.getResources().getDrawable(R.mipmap.check),
+                        null
+                );
+            } else {
+                viewHolder.tv.setTextColor(mContext.getResources().getColor(R.color.unselected));
+                viewHolder.tv.setCompoundDrawablesWithIntrinsicBounds(
+                        null, null, null, null);
+            }
+        }
+    }
+
+    class ViewHolder {
+        TextView tv;
     }
 }
