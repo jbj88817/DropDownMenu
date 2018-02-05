4ad56f5ba782e89242c73240bb124960c379d5b9
Bojie Jiang <jiangbojie@gmail.com>
2018-02-04 23:21:17 -0800

    Setup adapters

--- app/src/main/java/us/bojie/dropdownmenu/MainActivity.java
+++ app/src/main/java/us/bojie/dropdownmenu/MainActivity.java
@@ -3,9 +3,12 @@ package us.bojie.dropdownmenu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
     private int[] imgIds = {R.drawable.city, R.drawable.age, R.drawable.sex, R.drawable.sign};
 
     private int signPosition = 0;
+    private GirdDropDownAdapter mCityAdapter;
+    private ListDropDownAdapter mAgeAdapter;
+    private ListDropDownAdapter mSexAdapter;
+    private ConstellationAdapter mConstellationAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,24 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViews() {
         ListView lvCity = new ListView(this);
+        mCityAdapter = new GirdDropDownAdapter(this, Arrays.asList(citys));
         lvCity.setDividerHeight(0);
+        lvCity.setAdapter(mCityAdapter);
+
+        ListView lvAge = new ListView(this);
+        mAgeAdapter = new ListDropDownAdapter(this, Arrays.asList(ages));
+        lvAge.setDividerHeight(0);
+        lvAge.setAdapter(mAgeAdapter);
+
+        ListView lvSex = new ListView(this);
+        mSexAdapter = new ListDropDownAdapter(this, Arrays.asList(sexs));
+        lvSex.setDividerHeight(0);
+        lvSex.setAdapter(mSexAdapter);
+
+        View constellationView = getLayoutInflater().inflate(R.layout.layout_constellation, null);
+        GridView gridView = constellationView.findViewById(R.id.constellation);
+        TextView okTextView = constellationView.findViewById(R.id.ok);
+        mConstellationAdapter = new ConstellationAdapter(this, Arrays.asList(signs));
+        gridView.setAdapter(mConstellationAdapter);
     }
 }
